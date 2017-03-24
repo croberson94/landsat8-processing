f6f712948670c6ac347e113aa223143cecdc1879
croberson94 <crobshere@gmail.com>
2017-03-24 08:33:12 -0500

    add process

---
+++ process.py
@@ -0,0 +1,23 @@
+import numpy as np
+import cv2
+#from matplotlib import pyplot as plt
+
+def print_img():
+	#load the color image in grayscale 0
+	location =  '/Users/Charles/Downloads/LC80250392016062LGN00.tar/LC80250392016062LGN00/LC80250392016062LGN00_B2.TIF'
+	img = cv2.imread(location)
+	cv2.imshow('image_1',img)
+	b, g, r = cv2.split(img)
+	cv2.imshow('image_2',b)
+	cv2.imshow('image_3',g)
+	cv2.imshow('image_4',r)
+	k = cv2.waitKey()
+	#press ESC key to close window
+	while (k != 27):
+		k = cv2.waitKey()
+	cv2.destroyAllWindows()
+	return
+
+def main():
+	print_img()
+main()
