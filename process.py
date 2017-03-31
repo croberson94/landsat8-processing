b93eaa8816d8f6585757629420544749f39df18f
croberson94 <crobshere@gmail.com>
2017-03-30 21:33:32 -0500

    further addition

--- process.py
+++ process.py
@@ -7,31 +7,37 @@ import os
 
 def img_array():
 	path = 'c:/Users/Charles/Desktop/landsat8_images'
+	my_array = []
 	for (path, dirs, files) in os.walk(path):
 		#print(path)
 		#print(files)
 		for file in files:
 			if file.endswith('.TIF'):
 				thefile = os.path.join(path, file)
-				print(thefile)
-	return
+				#print(thefile)
+				my_array.append(thefile)
+	print(my_array)
+	return (my_array)
 	
-def print_img():
-	#load the color image in grayscale 0
-	#location = '*.tif'
-	img = cv2.imread(location)
-	cv2.imshow('image_1',img)
-	b, g, r = cv2.split(img)
-	cv2.imshow('image_2',b)
-	cv2.imshow('image_3',g)
-	cv2.imshow('image_4',r)
-	k = cv2.waitKey()
-	#press ESC key to close window
-	while (k != 27):
+def print_img(my_array):
+	for elt in my_array:
+		location = elt
+		img = cv2.imread(location)
+		cv2.imshow('image_1',img)
+		'''
+		b, g, r = cv2.split(img)
+		cv2.imshow('image_2',b)
+		cv2.imshow('image_3',g)
+		cv2.imshow('image_4',r)
+		'''
 		k = cv2.waitKey()
-	cv2.destroyAllWindows()
-	return
+		#press ESC key to close window
+		while (k != 27):
+			k = cv2.waitKey()
+		cv2.destroyAllWindows()
+		return
 
 def main():
-	img_array()
+	my_array = img_array()
+	print_img(my_array)
 main()
