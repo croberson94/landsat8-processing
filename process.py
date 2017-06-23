ef9dab94c9825205efdc64e5a3d04272d3c2dec5
croberson94 <crobshere@gmail.com>
2017-06-23 17:52:16 -0500

    script enabled

--- process.py
+++ process.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 import numpy as np
 import cv2
 import os
@@ -7,7 +9,7 @@ import pdb
 
 def	img_array(path='c:/Users/Charles/Desktop/landsat8_images'):
 	#State the path where the images are located
-	#path='/home/croberson/LSAT8'
+	path='/home/croberson/LSAT8'
 	my_array=np.array([])
 	#Walk through the path and grab the file location names
 	for(path,dirs,files) in os.walk(path):
@@ -20,28 +22,25 @@ def	img_array(path='c:/Users/Charles/Desktop/landsat8_images'):
 def	print_img(my_array):
 	#Define array clusters
 	#path='/home/melrobin/research/chrism/landsat8-processing/LSAT8/'
-	path='c:/Users/Charles/Desktop/landsat8_images'
+	path='/home/croberson/LSAT8'
 	blues=np.array([])
 	greens=np.array([])
 	reds=np.array([])
-	image_files=['LC80250392016062LGN00_B2.TIF','LC80250392016062LGN00_B8.TIF','LC80250392016062LGN00_B8.TIF']
-	img_red = cv2.imread(path+image_files[0])
-	img_green=cv2.imread(path+image_files[1])
-	img_blue=cv2.imread(path+image_files[2])
+	image_files=['LC80250392016062LGN00_B2.TIF','LC80250392016062LGN00_B4.TIF','LC80250392016062LGN00_B8.TIF']
+	img_red = cv2.imread(path+image_files[0], 0)
+	img_green = cv2.imread(path+image_files[1], 0)
+	img_blue = cv2.imread(path+image_files[2], 0)
 	#We need to verify the shape of the images to ensure integrity
 	#print img_red.shape, img_green.shape, img_blue.shape
-	img_red = cv2.imread("C:/Users/Charles/Desktop/landsat8_images/LC80250392016062LGN00_B2.TIF", 0)
-	print(img_red.shape)
-	img_green = cv2.imread("C:/Users/Charles/Desktop/landsat8_images/LC80250392016062LGN00_B4.TIF", 0)
-	print(img_green.shape)
-	img_zero = img_red - img_green
-	print(img_zero.shape)
+        red = cv2.imread('/home/croberson/LSAT8/LC80250392016062LGN00_B2.TIF', 0)
+        print(red)
+        print(red.shape)
 	return
 
 def	main():
 	#State the path where the images are located
 	#path='/home/melrobin/research/chrism/landsat8-processing/LSAT8'
-	path='c:/Users/Charles/Desktop/landsat8_images'
+	path='/home/croberson/LSAT8'
 	
 	#Create the array
 	my_array=img_array(path)
