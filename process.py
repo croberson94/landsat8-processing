3ae2275663f73a217b1de640ac43f59caaeb5520
croberson94 <crobshere@gmail.com>
2017-06-25 20:04:03 -0500

    picture acquired

--- process.py
+++ process.py
@@ -5,7 +5,7 @@ import pdb
 #function that creates 3D array
 #function that walks through the os,varies with user
 
-def	img_array(path='c:/Users/Charles/Desktop/landsat8_images'):
+def	img_array(path='c:/Users/Charles/Desktop/LSAT8'):
 	#State the path where the images are located
 	#path='/home/croberson/LSAT8'
 	my_array=np.array([])
@@ -20,7 +20,7 @@ def	img_array(path='c:/Users/Charles/Desktop/landsat8_images'):
 def	print_img(my_array):
 	#Define array clusters
 	#path='/home/melrobin/research/chrism/landsat8-processing/LSAT8/'
-	path='c:/Users/Charles/Desktop/landsat8_images'
+	path='c:/Users/Charles/Desktop/LSAT8'
 	blues=np.array([])
 	greens=np.array([])
 	reds=np.array([])
@@ -28,20 +28,35 @@ def	print_img(my_array):
 	img_red = cv2.imread(path+image_files[0])
 	img_green=cv2.imread(path+image_files[1])
 	img_blue=cv2.imread(path+image_files[2])
+	
 	#We need to verify the shape of the images to ensure integrity
 	#print img_red.shape, img_green.shape, img_blue.shape
-	img_red = cv2.imread("C:/Users/Charles/Desktop/landsat8_images/LC80250392016062LGN00_B2.TIF", 0)
-	print(img_red.shape)
-	img_green = cv2.imread("C:/Users/Charles/Desktop/landsat8_images/LC80250392016062LGN00_B4.TIF", 0)
-	print(img_green.shape)
-	img_zero = img_red - img_green
-	print(img_zero.shape)
+	# Each band that we need is used for different studies
+	# Band for terrain occlusion (2), water (3), and vegetation (4)
+	# Band 8 is for cloud coverage (supposed panochromatic)? will look into BIT for landsatlook quality images
+	
+	# if reading with cv2, use 1 for color, 0 for grayscale, -1 for alpha 
+	img_band2 = cv2.imread("C:/Users/Charles/Desktop/LSAT8/LC80250392016062LGN00_B2.TIF", 1)
+	print(img_band2.shape)
+	
+	# Resize the band if needed to view the image before saving
+	small_band2 = cv2.resize(img_band2, (500,500))
+	cv2.imshow('blue band', small_band2)
+	k = cv2.waitKey(0)
+	
+	# wait for escape key to be pressed to terminate 
+	if k == 27:
+		cv2.destroyAllWindows()
+	# save the image when the s key is pressed
+	elif k == ord('s'):
+		cv2.imwrite('band 2', small_band2)
+		cv2.destroyAllWindows()
 	return
 
 def	main():
 	#State the path where the images are located
 	#path='/home/melrobin/research/chrism/landsat8-processing/LSAT8'
-	path='c:/Users/Charles/Desktop/landsat8_images'
+	path='c:/Users/Charles/Desktop/LSAT8'
 	
 	#Create the array
 	my_array=img_array(path)
