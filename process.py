4a823f85502bf44a8599d2839f1ba4eb97955024
Melvin Robinson <melrobin@gmail.com>
2017-06-25 20:31:59 -0500

    Add command line argument necessity

--- process.py
+++ process.py
@@ -2,6 +2,7 @@ import numpy as np
 import cv2
 import os
 import pdb
+import sys
 #function that creates 3D array
 #function that walks through the os,varies with user
 
@@ -17,14 +18,14 @@ def	img_array(path='c:/Users/Charles/Desktop/LSAT8'):
 				my_array = np.append(my_array,thefile)
 	return(my_array)
 	
-def	print_img(my_array):
+def	print_img(my_array,path):
 	#Define array clusters
 	#path='/home/melrobin/research/chrism/landsat8-processing/LSAT8/'
-	path='c:/Users/Charles/Desktop/LSAT8'
+	#path='c:/Users/Charles/Desktop/LSAT8'
 	blues=np.array([])
 	greens=np.array([])
 	reds=np.array([])
-	image_files=['LC80250392016062LGN00_B2.TIF','LC80250392016062LGN00_B8.TIF','LC80250392016062LGN00_B8.TIF']
+	image_files=['LC80250392017096LGN00_B2.TIF','LC80250392017096LGN00_B8.TIF','LC80250392017096LGN00_B8.TIF']
 	img_red = cv2.imread(path+image_files[0])
 	img_green=cv2.imread(path+image_files[1])
 	img_blue=cv2.imread(path+image_files[2])
@@ -36,7 +37,7 @@ def	print_img(my_array):
 	# Band 8 is for cloud coverage (supposed panochromatic)? will look into BIT for landsatlook quality images
 	
 	# if reading with cv2, use 1 for color, 0 for grayscale, -1 for alpha 
-	img_band2 = cv2.imread("C:/Users/Charles/Desktop/LSAT8/LC80250392016062LGN00_B2.TIF", 1)
+	img_band2 = cv2.imread(path+'LC80250392017096LGN00_B2.TIF', 1)
 	print(img_band2.shape)
 	# if 1 is chosen, image will have three channels, split required?
 	
@@ -57,12 +58,15 @@ def	print_img(my_array):
 def	main():
 	#State the path where the images are located
 	#path='/home/melrobin/research/chrism/landsat8-processing/LSAT8'
-	path='c:/Users/Charles/Desktop/LSAT8'
-	
+	#path='c:/Users/Charles/Desktop/LSAT8'
+        if (len(sys.argv) != 2):
+           print "Not enough command line arguments provided...outie!"
+           exit()
+	path = sys.argv[1]
 	#Create the array
 	my_array=img_array(path)
 	
 	#Print the array
-	print_img(my_array)
+	print_img(my_array,path)
 
 main()
