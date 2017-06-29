436b42f90f97bb69a1dc008af01f03fbb60bc9b1
croberson94 <crobshere@gmail.com>
2017-06-29 16:49:19 -0500

    help menu added

--- process.py
+++ process.py
@@ -3,8 +3,12 @@ import cv2
 import os
 import pdb
 import sys
-#function that creates 3D array
-#function that walks through the os,varies with user
+import optparse
+
+# Options Menu
+parser = optparse.OptionParser()
+parser.add_option('-p', '--path', dest='path', help='Your Path')
+(options, args) = parser.parse_args()
 
 def	img_array(path='c:/Users/Charles/Desktop/LSAT8'):
 	#State the path where the images are located
@@ -29,8 +33,8 @@ def create_image(my_array,path):
 	img_red = cv2.imread(path+image_files[0])
 	img_green=cv2.imread(path+image_files[1])
 	img_blue=cv2.imread(path+image_files[2])
-        #Display the shapes to ensure that we have the correct spectral images
-        print img_red.shape,img_green.shape,img_blue.shape	
+    #Display the shapes to ensure that we have the correct spectral images
+    #print (img_red.shape,img_green.shape,img_blue.shape)
 	#We need to verify the shape of the images to ensure integrity
 	#print img_red.shape, img_green.shape, img_blue.shape
 	# Each band that we need is used for different studies
@@ -39,11 +43,11 @@ def create_image(my_array,path):
 	
 	# if reading with cv2, use 1 for color, 0 for grayscale, -1 for alpha 
 	img_band2 = cv2.imread(path+'LC80250392017096LGN00_B2.TIF', 1)
-        #Allocate and fill an output array
-        final_picture=np.empty(img_band2.shape)
-        final_picture[:,:,0]=img_red[:,:,0]
-        final_picture[:,:,1]=img_green[:,:,0]
-        final_picture[:,:,2]=img_blue[:,:,0]
+    #Allocate and fill an output array
+	final_picture = np.empty(img_band2.shape)
+	final_picture[:,:,0] = img_red[:,:,0]
+	final_picture[:,:,1] = img_green[:,:,0]
+	final_picture[:,:,2] = img_blue[:,:,0]
 	# if 1 is chosen, image will have three channels, split required?
 	
 	# Resize the band if needed to view the image before saving
@@ -64,14 +68,14 @@ def main():
 	#State the path where the images are located
 	#path='/home/melrobin/research/chrism/landsat8-processing/LSAT8'
 	#path='c:/Users/Charles/Desktop/LSAT8'
-    if (len(sys.argv) != 2):
-        print "Not enough command line arguments provided...outie!"
-        exit()
-    path = sys.argv[1]
+	if	(len(sys.argv) != 2):
+		print ("Not enough command line arguments provided...outie!")
+		exit()
+	path = sys.argv[1]
     #Create the array
-    my_array=img_array(path)
+	my_array=img_array(path)
 	
 	#Print the array
-    create_image(my_array,path)
+	create_image(my_array,path)
 
 main()
